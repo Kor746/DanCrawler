2b5f4ad2afbe3c5f337d71eb5de59c95b14cd0da
Dan <daniel@incubateinstitute.org>
2017-04-01 15:12:50 -0400

    Minor changes

--- config.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'cS6Txw3yxTydqHzXEA75Qz683'
-consumer_secret = 'Emi77bacTC8vZZNHGe7bqdSXJuLHxfbXPaZvptHj49uo7FwaM4'
-access_token = '306975143-NUbdKfLhAc93qur6p4qSuVlo7cz1o7HjLBDEwF9a'
-access_token_secret = 'R4uaAfqWcYXhij1c6123G7Lk13OphPQhP704Waz3fyjIz'
